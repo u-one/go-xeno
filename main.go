b2ae067f086df3c90ec609d5987c0a8336c0b6c3
u-one <backflip270bb@gmail.com>
2020-05-01 12:34:41 +0900

    Set random seed from time

--- main.go
+++ main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/u-one/go-xeno/xeno"
 )
 
 func init() {
-	// var seed int64 = 3
-	// var seed int64 = 1587981835 // 残り0まで
-	// var seed int64 = 1587984873 // 英雄が皇帝に見つかる
-	// var seed int64 = 1587991022
-	var seed int64 = 1588075429
-	//var seed int64 = time.Now().Unix()
+	var seed int64 = time.Now().Unix()
 	fmt.Println("Seed:", seed)
 	rand.Seed(seed)
 }
